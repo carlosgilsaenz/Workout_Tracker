853608d75dcffb2462768099ea460f619ee779e6
John Doe <john.doe@example.com>
2017-04-12 17:30:29 -0700

    1.1	Add read(), add display(), change structure of @OnClick

--- app/src/main/java/com/example/android/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/android/workouttracker/MainActivity.java
@@ -34,10 +34,15 @@ public class MainActivity extends AppCompatActivity {
     public void lastClicked() {
         SQLiteDatabase database = mDbHelper.getReadableDatabase();
 
-        //  Most logic performed on helper method
-        String results = getLastWorkout(database);
+        // Read from Database
+        Cursor cursor = read(database);
 
-        Toast.makeText(this, results, Toast.LENGTH_SHORT).show();
+        // Extra String from Cursor
+        String results = getLastWorkout(cursor);
+
+        displayToast(results);
+
+        database.close();
     }
 
     @Override
@@ -48,41 +53,6 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mDbHelper = new WorkoutDBHelper(this);
-    }
-
-    public String getLastWorkout(SQLiteDatabase db) {
-
-        // projection of data to grab
-        String[] projection = {
-                workoutEntry.COLUMN_DATE};
-
-        // Perform a query on the workout table
-        Cursor cursor = db.query(
-                workoutEntry.TABLE_NAME,   // The table to query
-                projection,            // The columns to return
-                null,                  // The columns for the WHERE clause
-                null,                  // The values for the WHERE clause
-                null,                  // Don't group the rows
-                null,                  // Don't filter by row groups
-                null);                   // The sort order
-
-        // Figure out the index of each column
-        int dateColumnIndex = cursor.getColumnIndex(workoutEntry.COLUMN_DATE);
-
-        String currentDate;
-
-        // Move to last row
-        if (cursor.moveToLast()) {
-            currentDate = getString(R.string.get_last_response_one) + cursor.getString(dateColumnIndex);
-        } else {
-            currentDate = getString(R.string.get_last_response_two);
-        }
-
-        // close all to save resources
-        cursor.close();
-        db.close();
-
-        return currentDate;
     }
 
     public void insertWorkout(SQLiteDatabase db) {
@@ -98,9 +68,58 @@ public class MainActivity extends AppCompatActivity {
 
         //  Checks if save is successful and prompts accordingly
         if (id == -1) {
-            Toast.makeText(this, R.string.insert_response_one, Toast.LENGTH_SHORT).show();
+            displayToast(getString(R.string.insert_response_one));
         } else {
-            Toast.makeText(this, R.string.insert_response_two, Toast.LENGTH_SHORT).show();
+            displayToast(getString(R.string.insert_response_two));
         }
+    }
+
+    /**
+     * For project requirement
+     */
+    public Cursor read(SQLiteDatabase db) {
+
+        // projection of data to grab
+        String[] projection = {
+                workoutEntry.COLUMN_DATE};
+
+        // Perform a query on the workout table
+        Cursor cursor = db.query(
+                workoutEntry.TABLE_NAME,   // The table to query
+                projection,            // The columns to return
+                null,                  // The columns for the WHERE clause
+                null,                  // The values for the WHERE clause
+                null,                  // Don't group the rows
+                null,                  // Don't filter by row groups
+                null);                   // The sort order
+
+        return cursor;
+    }
+
+    public String getLastWorkout(Cursor cursor) {
+
+        // Figure out the index of each column
+        int dateColumnIndex = cursor.getColumnIndex(workoutEntry.COLUMN_DATE);
+
+        String currentDate;
+
+        // Move to last row
+        if (cursor.moveToLast()) {
+            currentDate = getString(R.string.get_last_response_one) + cursor.getString(dateColumnIndex);
+        } else {
+            currentDate = getString(R.string.get_last_response_two);
+        }
+
+        // close to save resources
+        cursor.close();
+
+        return currentDate;
+    }
+
+    /**
+     * Suggested method for Toast Messages
+     */
+    public void displayToast(String string) {
+        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
     }
 }
